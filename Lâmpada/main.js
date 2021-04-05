436f7070041d99729468f4007c3f92035daaabbc
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-04-05 10:47:57 -0300

    Projeto Lâmpada

---
+++ Lâmpada/main.js
@@ -0,0 +1,17 @@
+
+const  lampada = document.getElementById("lampada");
+
+function ligarLampada() {
+ document.getElementById("lampada").setAttribute("src", "img/ligada.jpg");
+}
+
+function delisgarLampada() {
+    document.getElementById("lampada").setAttribute("src", "img/desligada.jpg");
+}
+
+function lapadaQuebrada(){
+    document.getElementById("lampada").setAttribute("src", "img/quebrada.jpg");
+    
+}
+
+
